9efce1a967feb7096ae8694f280861a3c83d24f3
Lee <User@DESKTOP-D57EIGB>
2018-12-08 01:21:18 -0500

    done mostly

--- metronome/src/metronome.c
+++ metronome/src/metronome.c
@@ -16,6 +16,7 @@
 #define ROWNUM 8
 
 #define MY_PULSE_CODE   _PULSE_CODE_MINAVAIL
+#define MY_PULSE_PAUSE   _PULSE_CODE_DISCONNECT
 
 int counter = 0;
 
@@ -26,8 +27,8 @@ typedef union {
 
 typedef struct DataTable
 {
-	int top; //time-signature-top (O)
-	int bottom; //time-signature-bottom
+	int top_sig; //time-signature-top (O)
+	int bot_sig; //time-signature-bottom
 	int interval; //Number of Intervals within each beat
 	char *pattern;
 } DataTableRow;
@@ -44,12 +45,14 @@ DataTableRow t[] = {
 };
 char data[255];
 int server_coid;
-
-
-int checkInt (const char *operand);
-void printOutput(char *output);
+name_attach_t *att;
+int rcvid;
+int curr;
+double space;
+char *pattern;
+void printPattern (char *output);
 void *childThread(void *arg);
-
+void *printThread(void *arg);
 //@Override int io_read()
 int io_read(resmgr_context_t *ctp, io_read_t *msg, RESMGR_OCB_T *ocb)
 {
@@ -103,9 +106,9 @@ int io_write(resmgr_context_t *ctp, io_write_t *msg, RESMGR_OCB_T *ocb)
 			small_integer = atoi(pause_msg);
 			if(small_integer >= 1 && small_integer <= 9){
 				//FIXME :: replace getprio() with SchedGet()
-				MsgSendPulse(server_coid, SchedGet(0,0,NULL), _PULSE_CODE_MINAVAIL, small_integer);
+				MsgSendPulse(server_coid, SchedGet(0,0,NULL), MY_PULSE_PAUSE, small_integer);
 			} else {
-				printf("Integer is not between 1 and 99.\n");
+				printf("Integer is not between 1 and 9.\n");
 			}
 		} else {
 			strcpy(data, buf);
@@ -134,132 +137,67 @@ int io_open(resmgr_context_t *ctp, io_open_t *msg, RESMGR_HANDLE_T *handle, void
 
 int main(int argc, char** argv) {
 
-	int j, rcvid;
-	int flag = 0;
-
-	double beats, time_top, pause_time, intervals;
-	double time_bottom;
-	double second_p, space;
-	char *output;
-	name_attach_t *att;
-	struct sigevent event;
-	struct itimerspec itime;
-	timer_t timer_id;
-	my_message_t msg;
-
+	double beats, top_sig, bot_sig, interval;
 	pthread_attr_t attr; // Thread attribute
 
-	// Checking the arguments
-	if(argc != 4) {
-		fprintf (stderr, "usage: program [beats-per-minute] [time-signature-top] [time-signature-bottom]\n");
-		exit (EXIT_FAILURE);
-	}
 
-	// Checking the valid input
-	if(checkInt(argv[1])){
-		fprintf (stderr, "Sorry. It must be a number.\n\n");
+
+
+
+	pthread_attr_init(&attr); // Initializes a default thread attribute
+	pthread_create(NULL, &attr, &childThread, NULL); // Create a new thread
+	//pthread_attr_destroy(&attr); // Destroy a thread attribute
+
+	if(argc != 4) {
+		fprintf (stderr, "usage: metronome beats-per-minute time-signature-top time-signature-bottom\n");
 		exit (EXIT_FAILURE);
 	}
 	beats = (double)atoi(argv[1]);
-
-	// Checking the valid input
-	if(checkInt(argv[2])){
-		fprintf (stderr, "Sorry. It must be a number.\n\n");
-		exit (EXIT_FAILURE);
-	}
-	time_top = (double)atoi(argv[2]);
-
-	// Checking the valid input
-	if(checkInt(argv[3])){
-		fprintf (stderr, "Sorry. It must be a number.\n\n");
-		exit (EXIT_FAILURE);
-	}
-	time_bottom = (double)atoi(argv[3]);
-
+	top_sig = (double)atoi(argv[2]);
+	bot_sig = (double)atoi(argv[3]);
 	if ((att = name_attach(NULL, "metronome", 0)) == NULL) {
+		perror("Name attach\n");
 		return EXIT_FAILURE;
 	}
 
-	for(j = 0; j < ROWNUM; j++){
-		if(time_top == t[j].top && time_bottom == t[j].bottom){
-			intervals = t[j].interval;
-			output = t[j].pattern;
-			flag = 0;
+
+	for(int i = 0; i < 8; i++){
+		if( top_sig == t[i].top_sig && bot_sig == t[i].bot_sig){
+			interval = t[i].interval;
+			pattern = t[i].pattern;
 			break;
-		}else
-			flag = 1;
-	}
-
-	if(flag == 1){
-		fprintf (stderr, "Error. Try again\n\n");
-		exit (EXIT_FAILURE);
-	}
-
-	second_p = (60 / beats) * time_top;
-
-	space = second_p / intervals;
-
-
-
-	event.sigev_notify = SIGEV_PULSE;
-	event.sigev_coid = ConnectAttach(ND_LOCAL_NODE, 0,att->chid,_NTO_SIDE_CHANNEL, 0);
-	event.sigev_priority = SchedGet(0,0,NULL);
-	event.sigev_code = MY_PULSE_CODE;
-	timer_create(CLOCK_REALTIME, &event, &timer_id);
-
-	itime.it_value.tv_sec = 0;
-	/* 500 million nsecs = .5 secs */
-	itime.it_value.tv_nsec = 1;
-	itime.it_interval.tv_sec = 0;
-	/* 500 million nsecs = .5 secs */
-	itime.it_interval.tv_nsec = space * 1000000000;
-	timer_settime(timer_id, 0, &itime, NULL);
-
-	while(1){
-
-		pthread_attr_init(&attr); // Initializes a default thread attribute
-		pthread_create(NULL, &attr, &childThread, NULL); // Create a new thread
-		pthread_attr_destroy(&attr); // Destroy a thread attribute
-
-		rcvid = MsgReceivePulse(att->chid, &msg, sizeof(msg), NULL);
-		if (rcvid == 0) { /* we got a pulse */
-			printf("Small Integer: %d\n", msg.pulse.value.sival_int);
-			if (msg.pulse.code == MY_PULSE_CODE) {
-				fflush(stdout);
-				printOutput(output);
-			} /* else other pulses ... */
+		}else{
+			perror("Invalid combo\n");
+			exit(EXIT_FAILURE);
 		}
 
 	}
+
+	space = (60 / beats) * top_sig/interval;
+	pthread_attr_init(&attr);
+	pthread_create(NULL, &attr, &printThread, NULL);
+
+
+
+	while(1){
+
+	}
+
+
 	printf("\n%f\n", space);
 	return EXIT_SUCCESS;
 }
 
-/*******************************************************************************
- * checkInt( ): int
- ******************************************************************************/
-int checkInt (const char *num){
-
-	/* Validate input as a number */
-	const char *check = num;
-	while(*check != '\0'){
-		if(!isdigit(*check)){
-			return 1;
-		}
-		check++;
-	}
-	return 0;
-}
-
-void printOutput(char *output){
-	if(counter == strlen(output))
-		counter = 0;
-	if(counter == 0){
-		printf("%c%c", output[counter], output[counter+1]);
-		counter = 2;
+void printPattern(char *pattern){
+	if(curr == 0){
+		printf("%c%c",pattern[0], pattern[1]);
+		curr = 2;
+	}else if(curr == strlen(pattern)){
+		curr = 0;
 	}else{
-		printf("%c", output[counter]);
-		counter++;
+		printf("%c",pattern[curr]);
+		curr++;
+
 	}
 }
 /*******************************************************************************
@@ -288,6 +226,42 @@ void *childThread(void *arg){
 	while(1) {
 		ctp = dispatch_block(ctp);
 		dispatch_handler(ctp);
+	}
+}
+
+void *printThread(void *arg){
+	struct sigevent event;
+	struct itimerspec itime;
+	timer_t timer_id;
+	my_message_t msg;
+
+
+	event.sigev_notify = SIGEV_PULSE;
+	event.sigev_coid = ConnectAttach(ND_LOCAL_NODE, 0,att->chid,_NTO_SIDE_CHANNEL, 0);
+	event.sigev_priority = SchedGet(0,0,NULL);
+	event.sigev_code = MY_PULSE_CODE;
+	timer_create(CLOCK_REALTIME, &event, &timer_id);
+
+	itime.it_value.tv_sec = 0;
+	/* 500 million nsecs = .5 secs */
+	itime.it_value.tv_nsec = 1;
+	itime.it_interval.tv_sec = 0;
+	/* 500 million nsecs = .5 secs */
+	itime.it_interval.tv_nsec = space * 1000000000;
+	timer_settime(timer_id, 0, &itime, NULL);
+
+
+	while(1){
+		rcvid = MsgReceivePulse(att->chid, &msg, sizeof(msg), NULL);
+		if (rcvid == 0) { /* we got a pulse */
+			if (msg.pulse.code == MY_PULSE_CODE) {
+				fflush(stdout);
+				printPattern(pattern);
+			} else if(msg.pulse.code == MY_PULSE_PAUSE){
+				sleep(4);
+			}
+		}
+
 	}
 }
 
